f76f4b167246ca2e704f8dadfb1c07c33e9c016c
wangdong <wangdong@zhuanzhuan.com>
2020-04-08 16:47:17 +0800

    Initial Commit

---
+++ TestCategory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCategory
+//
+//  Created by wangzelong on 2020/4/8.
+//  Copyright © 2020 wangzelong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestCategory/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TestCategory
+//
+//  Created by wangzelong on 2020/4/8.
+//  Copyright © 2020 wangzelong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
